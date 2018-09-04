b5a33b247a65a79450557584edd5c896c8b2c12d
PhuLN <mtlplnguyen@gmail.com>
2018-09-04 19:56:06 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import 'buefy/lib/buefy.css';
 import Vue from 'vue';
+import Buefy from 'buefy';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
